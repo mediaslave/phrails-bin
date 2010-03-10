33079173c89a9233ffca94a008d1140c0f82b9bc
Justin Palmer <justin@mediaslave.net>
2010-03-09 16:03:24 -0800

    Broke off some of the functions from phrails into a helpers/directory.php file and added support for script/generate controller

---
+++ helpers/directory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Create on directory and output some information
+ *
+ * @param string $path 
+ * @return void
+ * @author Justin Palmer
+ */
+function create_directory($path){
+	if(is_dir($path)){
+		print "Exists: $path\n";
+	}else{
+		mkdir($path);
+		print "Created: $path\n";
+	}
+}
+/**
+ * Copy a file from the source to the correct location.
+ *
+ * @param string $src 
+ * @param string $dest 
+ * @return void
+ * @author Justin Palmer
+ */
+function create_file($src, $dest){
+	$dest = $dest . '/' . $src;
+	$src = CUR_DIR . '/__files__/' . $src;
+	if(is_file($dest)){
+		print "Exists: $dest\n";
+	}else{
+		copy($src, $dest);
+		print "Created: $dest\n";
+	}
+}
