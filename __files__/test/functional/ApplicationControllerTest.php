6909d30f38ef873996d1b8c864199cb9b8331a93
Justin Palmer <justin@mediaslave.net>
2010-10-06 12:22:56 -0700

    Phrails additions to test and generate now generates tests properly.

---
+++ __files__/test/functional/ApplicationControllerTest.php
@@ -0,0 +1,19 @@
+<?php	
+/**
+ * PageBlock
+ */
+/**
+ * Namespace for the controllers
+ */
+namespace \Tests\Functional;
+/**
+ * ClassBlock
+ */
+class ApplicationControllerTest extends \PHPUnit_Extensions_SeleniumTestCase
+{
+	public function setUp()
+	{
+		//$this->setBrowser('*firefox /path/to/firefox-bin');
+		//$this->setBrowserUrl('http://your-url/');
+	}
+}
