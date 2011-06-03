570f0930834f2ca18e93f301d07a87830a7ba335
Justin Palmer <justin@mediaslave.net>
2011-06-02 17:11:56 -0700

    Updated boot.php

--- __files__/config/boot.php
+++ __files__/config/boot.php
@@ -5,6 +5,8 @@ include 'config/environment.php';
 //Load the inflections initializer;
 include_all_in_folder(__DIR__ . '/initializers');
 
+Registry::set('pr-real-install-path', realpath(__DIR__ .  '/../'));
+
 //load the db settings
 include Registry::get('pr-framwork-install-path') . '/db.php';
 
