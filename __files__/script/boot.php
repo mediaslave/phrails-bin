a502659679d252169085f1d835c3e13b8a88b5e0
Justin Palmer <justin@mediaslave.net>
2011-11-10 14:50:08 -0800

    Added a script/console.
    Requires phpsh

--- __files__/script/boot.php
+++ __files__/script/boot.php
@@ -2,7 +2,7 @@
 //Register the autoload function for the command line scripts
 spl_autoload_register('cmd_autoload');
 //Boot up the framework.
-include __DIR__ . '/../../../../config/boot.php';
+include __DIR__ . '/../config/boot.php';
 //Include the migrations folder.
 add_include_directory(__DIR__ . '/../db/migrations');
 //Register the autoload for migrations.
